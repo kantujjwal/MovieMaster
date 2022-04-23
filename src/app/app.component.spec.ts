15c9b832fd68536252240d4ecb7b9dbe63bda646
Ujjwal Kant <ujjwal2154@gmail.com>
2022-04-23 14:38:26 +0530

    Renamed Angular Demo to MovieMaster

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularDemo'`, () => {
+  it(`should have as title 'MovieMaster'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularDemo');
+    expect(app.title).toEqual('MovieMaster');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularDemo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MovieMaster app is running!');
   });
 });
