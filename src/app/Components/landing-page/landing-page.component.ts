408aa46e4f4da418e4d4510a9880d086cb2c95f3
Ujjwal Kant <ujjwal2154@gmail.com>
2022-04-23 14:35:32 +0530

    fixes

--- src/app/Components/landing-page/landing-page.component.ts
+++ src/app/Components/landing-page/landing-page.component.ts
@@ -11,16 +11,6 @@ export class LandingPageComponent implements OnInit, OnDestroy {
 
   menuNav = [{name:"Latest Movies", link:'latest-movies'}, {name:"Contact Us", link:'contactus'}, {name:"Movie List", link:'movie-list'}]
 
-  fillerContent = Array.from(
-    {length: 50},
-    () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  );
-
   private _mobileQueryListener: () => void;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
